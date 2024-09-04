23cec92cf9de5b4f308eeefad886cdb5096e6f5f
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-09-04 19:12:41 -0400

    Change variable names from host01/02 to node01/02

--- scripts/sshTunnels.conf.bash
+++ scripts/sshTunnels.conf.bash
@@ -12,19 +12,19 @@ target="$(basename ${zero%.bash})"
 
 
 cat << EoFsshTunnels > "$medir/../$target"
-### Connection from magaox${suffix01} to magaox${suffix02} (hostname mx10ts)
+### Connection from magaox${suffix01} to magaox${suffix02} (hostname $node01)
 ### - local port set to -1 so a direct connection is made to the remote
 ###   server instead of to local sshDigger tunnel @localhost:<localPort>
 ### - compress has been removed, as it is not used in this case
 [magaox${suffix01}_02]
-remoteHost = $host02
+remoteHost = $node02
 remotePort = 7624
 localPort  = -1
 
-### Connection from magaox${suffix02} to magaox${suffix01} (hostname mx11ts)
+### Connection from magaox${suffix02} to magaox${suffix01} (hostname $node01)
 ### - Same as above
 [magaox${suffix02}_01]
-remoteHost = $host01
+remoteHost = $node01
 remotePort = 7624
 localPort  = -1
 EoFsshTunnels
