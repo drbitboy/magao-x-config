fa27f182bb42807f21b6269fc4204c1e8ca67095
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-09-04 18:53:54 -0400

    Make sshTunnels script work; add common strings

---
+++ scripts/common.bash
@@ -0,0 +1,12 @@
+
+
+suffix01=01
+role01=magaox$suffix01
+host01=mx11ts
+
+suffix02=02
+role02=magaox$suffix02
+host02=mx10ts
+
+isPrefix=isMagAOX
+dvrPrevix=dvr
--- scripts/sshTunnels.conf.bash
+++ scripts/sshTunnels.conf.bash
@@ -1,16 +1,31 @@
-### Connection from magaox01 to magaox02 (hostname mx10ts)
+#!/usr/bin/env bash
+
+########################################################################
+### /scripts/sshTunnels.conf.bash
+########################################################################
+
+
+
+zero="$0"
+medir="$(dirname "$zero")"
+target="$(basename ${zero%.bash})"
+
+
+cat << EoFsshTunnels > "$medir/../$target"
+### Connection from magaox${suffix01} to magaox${suffix02} (hostname mx10ts)
 ### - local port set to -1 so a direct connection is made to the remote
 ###   server instead of to local sshDigger tunnel @localhost:<localPort>
 ### - compress has been removed, as it is not used in this case
-[magaox01_02]
-remoteHost = mx10ts
+[magaox${suffix01}_02]
+remoteHost = $host02
 remotePort = 7624
 localPort  = -1
 
-### Connection from magaox02 to magaox01 (hostname mx11ts)
+### Connection from magaox${suffix02} to magaox${suffix01} (hostname mx11ts)
 ### - Same as above
-[magaox02_01]
-remoteHost = mx11ts
+[magaox${suffix02}_01]
+remoteHost = $host01
 remotePort = 7624
 localPort  = -1
+EoFsshTunnels
 
