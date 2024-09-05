9bfc62168b48387892100e854527e1bb795842d4
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-09-05 18:25:33 -0400

    Add script to create individual driver .conf file

--- scripts/dvr.conf.bash
+++ scripts/dvr.conf.bash
@@ -1,4 +1,27 @@
+#!/usr/bin/env bash
+
+one="$1"
+source "$(dirname "${BASH_SOURCE[0]}")"/node_setup.bash $one
+
+dvrID="$2"
+
+[ "$mag_node" ] \
+&& echo "$dvrID" | grep -q '^[0-9a-f][0-9a-f][0-9a-f]$' \
+|| ( echo "Usage  $0 A|B <driver ID as three lowercase hex digits>" && false ) \
+|| unset mag_node
+
+if [ "$mag_node" ] ; then
+  cat << EoFdvr
+### INDI driver $mag_dvrPrefix$dvrID
+### - created by script ${BASH_SOURCE[0]}
+### - from PWD=$PWD
+
 myVal=val
-otherDevName=maths_2
+otherDevName=$mag_otherdvrPrefix$dvrID
 otherValName=val
+EoFdvr
+else
+  false
+fi
+
 
