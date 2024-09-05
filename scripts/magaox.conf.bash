dd80a52a3648ec2d497f95b3af2c16564adeea37
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-09-05 19:40:08 -0400

    Finish script to create magaox.conf

--- scripts/magaox.conf.bash
+++ scripts/magaox.conf.bash
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+########################################################################
+### /scripts/magaox.conf.bash
+########################################################################
+
+zero="${BASH_SOURCE[0]}"
+medir="$(dirname "$zero")"
+target="$(basename ${zero%.bash})"
+
+cat << EoFsshTunnels > "$medir/../$target"
 indiserver_ctrl_fifo = /opt/MagAOX/drivers/fifos/indiserver.ctrl
+EoFsshTunnels
