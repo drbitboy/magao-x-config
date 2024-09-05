803d83a6bb493ad789e7bf963f51838b3a93606d
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2024-09-04 20:32:26 -0400

    Expand /scripts/common.bash; add comments

--- scripts/common.bash
+++ scripts/common.bash
@@ -1,12 +1,30 @@
 
+### /scripts/common.bash
+### - contains the suffixes for the two roles/INDI servers/drivers
+### - also the node names
+
+### Suffix for the two roles
 
 suffix01=01
-role01=magaox$suffix01
-node01=mx11ts
-
 suffix02=02
+
+### Roles for the two nodes
+
+role01=magaox$suffix01
 role02=magaox$suffix02
+
+### Nodes (hostnames)
+
+node01=mx11ts
 node02=mx10ts
 
-isPrefix=isMagAOX
-dvrPrefix=dvr
+### The "other" node
+
+othernode01=$node02
+othernode02=$node01
+
+### INDI server names:  isMagAOX01; isMagAOX02
+### Driver prefix without suffix:  dvr01; dvr02
+
+isPrefixNoSuffix=isMagAOX
+dvrPrefixNoSuffix=dvr
