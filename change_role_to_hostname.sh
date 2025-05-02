cbd76e55a3f0cb5e9e50df456b3d3a0cab2c89d9
Brian T. Carcich <Brian.T.Carcich@gmail.com>
2025-05-02 12:33:53 -0400

    Make hostname-role change more robust

--- change_role_to_hostname.sh
+++ change_role_to_hostname.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+hn="$(hostname)"
+pltxt="proclist_$hn.txt"
+
+( cd "$(dirname "$0")" \
+&& [ "${PWD}" == "/opt/MagAOX/config" ] \
+|| ( "ERROR:  script [$0] is not in /opt/MagAOX/config/; $0 exiting ..." && false ) \
+) || exit 1
+
+[ -r "$pltxt" ] \
+|| ( "ERROR:  hostnamed-proclist [$pltxt] is not readable; $0 exiting ..." && false ) \
+|| exit 2
+
+echo -n "Old role:  " && cat /etc/profile.d/magaox_role.sh
+
 sudo \
   sed -i \
   -e "s/export *MAGAOX_ROLE=.*/export MAGAOX_ROLE=$(hostname)/" \
   /etc/profile.d/magaox_role.sh
 
+echo -n "New role:  " && cat /etc/profile.d/magaox_role.sh
